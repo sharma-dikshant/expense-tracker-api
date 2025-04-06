edbf83e5d94ef6fb6c0b9e9aae166f1b7b97270e
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-04-06 21:48:52 +0530

    connect to database

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config({ path: "./config.env" });
 
 const app = express();
+
+const DB = process.env.DATABASE_URL.replace(
+  "<db_password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    //options
+  })
+  .then((con) => {
+    console.log("DB connected! ");
+  });
 
 app.get("/", (req, res) => {
   res.send("hello");
