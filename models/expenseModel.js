2d4cab5aa562a941c7b9feeddacf3406d0bb2107
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-04-21 14:23:36 +0530

    sets totalPrice as virtual property

--- models/expenseModel.js
+++ models/expenseModel.js
@@ -1,28 +1,34 @@
 const mongoose = require("mongoose");
 
-const expenseSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide a name"],
-    lowercase: true,
+const expenseSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name"],
+      lowercase: true,
+    },
+    unitPrice: {
+      type: Number,
+      required: [true, "Please provide an amount"],
+    },
+    quantity: {
+      type: Number,
+      required: [true, "Please provide a quantity"],
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+    user: {
+      type: String,
+      required: [true, "No user for this expense"],
+    },
   },
-  unitPrice: {
-    type: Number,
-    required: [true, "Please provide an amount"],
-  },
-  quantity: {
-    type: Number,
-    required: [true, "Please provide a quantity"],
-  },
-  totalPrice: Number,
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-  user: {
-    type: String,
-    required: [true, "No user for this expense"],
-  },
+  { toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+expenseSchema.virtual("totalPrice").get(function () {
+  return this.quantity * this.unitPrice;
 });
 
 const Expense = mongoose.model("Expense", expenseSchema);
