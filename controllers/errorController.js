8e83213e42e3206ee044836cc401534aaee07317
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-04-25 14:43:47 +0530

    error handling 3: adding seperate error for development and production env

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,6 +1,33 @@
-module.exports = (err, req, res, next) => {
+const sendDevError = (err, res) => {
   return res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
+    stack: err.stack,
+    error: err,
   });
+};
+const sendProdError = (err, res) => {
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.log("ERR 💥 something went very wrong", err);
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: "something went very wrong! Please try again later",
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "development") {
+    sendDevError(err, res);
+  } else {
+    sendProdError(err, res);
+  }
 };
