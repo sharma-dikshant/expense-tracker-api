7154593ca2e438bd0672b02cdb9dd087635489bb
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-04-29 08:57:52 +0530

    handled mongoose errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,5 @@
+const AppError = require("../utils/appError");
+
 const sendDevError = (err, res) => {
   return res.status(err.statusCode).json({
     status: err.status,
@@ -21,9 +23,32 @@ const sendProdError = (err, res) => {
   }
 };
 
+const handleCastError = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldError = (err) => {
+  const field = Object.keys(err.keyValue)[0];
+  const value = err.keyValue[field];
+  const message = `Duplicate field value: ${value}. Please use another value for ${field}`;
+  return new AppError(message, 400);
+};
+
+const handleValidationError = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `invalid input data: ${errors.join(". ")}`;
+
+  return new AppError(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
+
+  if (err.name === "CastError") err = handleCastError(err);
+  if (err.code === 11000) err = handleDuplicateFieldError(err);
+  if (err.name === "ValidationError") err = handleValidationError(err);
 
   if (process.env.NODE_ENV === "development") {
     sendDevError(err, res);
