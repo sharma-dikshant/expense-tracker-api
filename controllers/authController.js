b45d8e4bfd79c3ca1314c288af6c91a68ae11641
sharma-dikshant <dikshantsharma2005@gmail.com>
2025-05-11 11:14:42 +0530

    fix: fixed cookies options for logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -35,12 +35,16 @@ exports.login = catchAsync(async (req, res, next) => {
 });
 
 exports.logout = (req, res, next) => {
-  res.cookie("jwt", "loggedout", {
-    expires: new Date(Date.now() + 10 * 1000),
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+  const cookiesOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIES_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
     httpOnly: true,
-  });
+    secure: process.env.NODE_ENV === "production",
+  };
+
+  if (process.env.NODE_ENV === "production") cookiesOptions.sameSite = "None";
+  res.cookie("jwt", "loggedout", cookiesOptions);
   res.status(200).json({ status: "success" });
 };
 
